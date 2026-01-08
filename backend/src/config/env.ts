fcd8b3a0ef1f662586bfdfd319de7a268cd13a86
Akhilesh Tiwari <10akhil.t@gmail.com>
2026-01-08 21:26:44 +0530

    chore: updated config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,4 +5,6 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 3000,
   geminiApiKey: process.env.GEMINI_API_KEY,
+  openRouterApiKey: process.env.OPEN_ROUTER_KEY,
+  frontendUrl: process.env.FRONTEND_URL,
 };
