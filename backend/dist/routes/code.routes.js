402fca93add01b329fa1f1b488248f3cf361dabd
Akhilesh Tiwari <10akhil.t@gmail.com>
2026-08-06 23:37:42 +0530

    chore: remove compiled backend build artifacts

--- backend/dist/routes/code.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const asyncHandler_1 = require("../middleware/asyncHandler");
-const execute_controller_1 = require("../controllers/execute.controller");
-const router = (0, express_1.Router)();
-router.post("/execute", (0, asyncHandler_1.asyncHandler)(execute_controller_1.executeCode));
-exports.default = router;
